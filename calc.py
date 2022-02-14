8e9a664cf4d46b21402389b07c5c555fc6dba3b7
yusufsuganda23 <yusufshoopy62@gmail.com>
2022-02-14 20:35:07 +0700

    Menambahkan function perkalian serta branch perkalian

--- calc.py
+++ calc.py
@@ -6,8 +6,13 @@ def pengurangan(a,b):
     total = a-b
     return total
 
+def perkalian(a,b):
+    total = a*b
+    return total
+
 def main():
     print(penambahan(10,5))
     print(pengurangan(10,5))
+    print(perkalian(10,5))
 
 main()
