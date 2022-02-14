55c48a0bc48bcde7666c07d1924c091522697b39
yusufsuganda23 <yusufshoopy62@gmail.com>
2022-02-14 20:27:43 +0700

    Menambahkan function pengurangan dan branch pengurangan

--- calc.py
+++ calc.py
@@ -2,7 +2,12 @@ def penambahan(a,b):
     total = a+b
     return total
 
+def pengurangan(a,b):
+    total = a-b
+    return total
+
 def main():
     print(penambahan(10,5))
+    print(pengurangan(10,5))
 
 main()
